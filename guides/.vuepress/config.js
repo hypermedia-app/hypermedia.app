238bcd70fa8402e9a5f93d8be00e92e9d6ff1529
tpluscode <tpluscode@users.noreply.github.com>
2019-05-02 09:55:04 +0200

    initial vuepress structure

---
+++ guides/.vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    title: 'Building hypermedia apps',
+    description: 'Guides to using Hydra on back end and front end',
+    repo: 'hypermedia.app/building.hypermedia.app',
+    docsDir: 'guides',
+    editLinks: true,
+    editLinkText: 'Edit Page',
+    lastUpdated: 'Last Updated',
+    nav: [
+        { text: 'Home', link: '/' },
+        { text: 'Hydrofoil', link: '/hydrofoil/' },
+        { text: 'Argolis', link: '/argolis/' },
+        { text: 'hydra-box', link: '/hydra-box/' },
+    ]
+};
---
+++ guides/readme.md
@@ -0,0 +1,11 @@
+---
+home: true
+heroImage: https://github.com/HydraCG/hydra-cg.com/raw/master/img/logo.png
+features:
+  - title: Hydrofoil - Single Page App scaffolding
+    details: Reduces boilerplate using a set of web components
+  - title: hydra-box - rapid Hydra development in node.js
+    details: Simple yet powerful express.js middleware
+  - title: Argolis - .NET Hydra server on top of Nancy
+    details: Develop like you always would, serve RDF with Hydra controls 
+---
